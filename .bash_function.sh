cd00710dc9ec144c9fc7633b2e8657ccdc47a423
oushu1lilulu1 <li.lulu@oushu.io>
2022-03-21 16:23:37 +0800

    update

--- .bash_function.sh
+++ .bash_function.sh
@@ -14,11 +14,13 @@ hdfs dfs -rm -r /hawq*
 rm -rf /usr/local/oushu/log/oushudb/*
 rm -rf /tmp/magma_catalog/*
 rm -rf /tmp/magma_data/*
+rm -rf ~/hawq-data-directory/
 }
 
 function oushu_killall {
 ps -ef | grep postgres | grep -v grep | awk '{print $2}'| xargs kill -9
 ps -ef | grep magma | grep -v grep | awk '{print $2}'| xargs kill -9
+oushudb ps
 }
 
 function v {
@@ -26,3 +28,59 @@ file_name=${1#*-}
 vim ${file_name}
 }
 
+function reinit {
+echo "************************kill all***************************"
+oushu_killall 
+echo "**************************clean****************************"
+oushu_clean 
+echo "**************************Iinit****************************"
+oushu_init
+}
+
+function oushu_make {
+dir=`pwd`
+res=expr index ${dir} "debug"
+if [ 0=${res} ];then
+	echo "You are not in debug directory,please checkout to debug directory and retry!"
+else
+	make debug -j8
+fi
+}
+
+function log {
+vim /usr/local/oushu/log/oushudb/
+}
+
+function deletevm {
+python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lululi |awk '{print $4}' |xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --deletevm
+
+rm -rf /Users/lululi/apps/ci-publiccloud/setup_machine/clusterid
+}
+
+function vm1 {
+ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
+password_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $12}'))
+for x in ${ip_array[@]};
+do
+echo $x
+done
+echo "sshpass -p ${password_array[1]} ssh -o StrictHostKeyChecking=no  oushu@${ip_array[1]}" 
+sshpass -p ${password_array[1]}  ssh oushu@${ip_array[1]} 
+}
+function vm2 {
+ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
+password_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $12}'))
+echo "sshpass -p ${password_array[2]} ssh -o StrictHostKeyChecking=no   oushu@${ip_array[2]}"
+sshpass -p ${password_array[2]}  ssh oushu@${ip_array[2]}
+}
+function vm3 {
+ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
+password_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $12}'))
+echo "sshpass -p ${password_array[3]} ssh -o StrictHostKeyChecking=no   oushu@${ip_array[3]}"
+sshpass -p ${password_array[3]}  ssh oushu@${ip_array[3]}
+}
+
+function latestvm {
+id=`cat $ci_dir/setup_machine/clusterid`
+：
+}
--- .bashrc
+++ .bashrc
@@ -8,13 +8,31 @@ source /opt/dependency-Darwin/package/env.sh
 export HADOOP_HOME=/usr/local/Cellar/hadoop/3.3.1/libexec 
 export PATH=$PATH:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin
 export JAVA_HOME=/usr/local/Cellar/openjdk@8/1.8.0+312/libexec/openjdk.jdk/Contents/Home
-source /Users/lululi/.bash_function.sh
 source /usr/local/oushu/oushudb/oushudb_path.sh
 export DYLD_LIBRARY_PATH=/opt/dependency-Darwin/package/lib:$DYLD_LIBRARY_PATH
-alias goproxy='export http_proxy=http://127.0.0.1:4780 https_proxy=http://127.0.0.1:4780'
-alias disproxy='unset http_proxy https_proxy'
 alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias vimrc='vim ~/.vimrc'
 alias bashrc='vim ~/.bashrc'
 alias bash_func='vim ~/.bash_function.sh'
 alias source_rc='source  ~/.bashrc'
+alias oushu_init='oushudb init cluster --with_magma -a'
+source /Users/lululi/.bash_function.sh
+source /Users/lululi/.git-completion.bash
+
+mkdir -p /tmp/magma_catalog
+mkdir -p /tmp/magma_data
+alias lgt="ssh lululi@101.43.188.6"
+alias oushu='cd /usr/local/oushu/'
+export PATH=/usr/local/oushu/lavam/bin/:$PATH
+export LAVA_USER="lilulu@oushutest"
+export LAVA_PASSWORD="qminet8r5h"
+alias startvm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --multicreate 3,3"
+alias listvm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu|awk '{print $4}'"
+alias listci_vm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lll|awk '{print $4}'"
+alias startvm_debug="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --multicreate 3,3 --testbinary debug"
+export PATH=/usr/local/oushu/lavam/bin:$PATH
+alias ci='cd /Users/lululi/apps/ci-publiccloud/setup_machine/python/'
+alias vm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm"
+export ci_dir="/Users/lululi/apps/ci-publiccloud/"
+export RUN_UNITTEST=NO
+export PATH=/usr/local/oushu/lavam/bin:$PATH
