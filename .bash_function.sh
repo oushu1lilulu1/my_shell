9299f7cbc8a90b1b10fe622ceaf0fcdd6fdcc871
oushu1lilulu1 <li.lulu@oushu.io>
2022-09-18 16:53:54 +0800

    update

--- .bash_function.sh
+++ .bash_function.sh
@@ -10,17 +10,31 @@ function git_branch {
 export PS1='\u@\h \[\033[01;36m\]\W\[\033[01;32m\]$(git_branch)\[\033[00m\] \$ '
 
 function oushu_clean {
+hdfs dfs -rm -r /hdfs*
 hdfs dfs -rm -r /hawq*
 rm -rf /usr/local/oushu/log/oushudb/*
-rm -rf /tmp/magma_catalog/*
-rm -rf /tmp/magma_data/*
+rm -rf /Users/lululi/magma/magma_catalog/*
+rm -rf /Users/lululi/magma/magma_data/*
+rm -rf /Users/lululi/magma/magma_catalog1/*
+rm -rf /Users/lululi/magma/magma_catalog2/*
+rm -rf /Users/lululi/magma/magma_catalog3/*
 rm -rf ~/hawq-data-directory/
 }
 
 function oushu_killall {
 ps -ef | grep postgres | grep -v grep | awk '{print $2}'| xargs kill -9
 ps -ef | grep magma | grep -v grep | awk '{print $2}'| xargs kill -9
-oushudb ps
+if [ "$dbversion" = '50' ];then
+    oushudb ps
+elif [ "$dbversion" = '49' ];then
+    hawq ps
+fi
+}
+
+function ssh_vm {
+   	ip=$1
+	password=$2
+sshpass -p ${password}  ssh -o StrictHostKeyChecking=no oushu@${ip} 
 }
 
 function v {
@@ -28,34 +42,52 @@ file_name=${1#*-}
 vim ${file_name}
 }
 
-function reinit {
+function free_shared_mem {
+ipcs -m | cut -f2 -d' ' | xargs -I {} sudo ipcrm -m {}
+}
+
+function reinitmagma {
+
 echo "************************kill all***************************"
 oushu_killall 
 echo "**************************clean****************************"
 oushu_clean 
+#echo "**************************free****************************"
+#free_shared_mem
 echo "**************************Iinit****************************"
 oushu_init
 }
 
 function oushu_make {
 dir=`pwd`
-res=expr index ${dir} "debug"
+res=expr index ${dir} "build"
 if [ 0=${res} ];then
-	echo "You are not in debug directory,please checkout to debug directory and retry!"
+	echo "You are not in build directory,please checkout to build directory and retry!"
 else
 	make debug -j8
 fi
 }
 
 function log {
-vim /usr/local/oushu/log/oushudb/
+cd /usr/local/oushu/log/oushudb/
 }
 
 function deletevm {
-python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lululi |awk '{print $4}' |xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --deletevm
+if [ -x $1 ];then
 
-rm -rf /Users/lululi/apps/ci-publiccloud/setup_machine/clusterid
+	python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lululi |awk '{print $4}' |xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --deletevm
+
+	rm -rf /Users/lululi/apps/ci-publiccloud/setup_machine/clusterid
+ else
+
+	for x in ${@};
+	do 
+		python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --deletevm $x
+	done
+
+fi
 }
+
 
 function vm1 {
 ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
@@ -65,22 +97,151 @@ do
 echo $x
 done
 echo "sshpass -p ${password_array[1]} ssh -o StrictHostKeyChecking=no  oushu@${ip_array[1]}" 
-sshpass -p ${password_array[1]}  ssh oushu@${ip_array[1]} 
+sshpass -p ${password_array[1]}  ssh -o StrictHostKeyChecking=no oushu@${ip_array[1]} 
 }
 function vm2 {
 ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
 password_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $12}'))
 echo "sshpass -p ${password_array[2]} ssh -o StrictHostKeyChecking=no   oushu@${ip_array[2]}"
-sshpass -p ${password_array[2]}  ssh oushu@${ip_array[2]}
+sshpass -p ${password_array[2]}  ssh -o StrictHostKeyChecking=no oushu@${ip_array[2]}
 }
 function vm3 {
 ip_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $8}'))
 password_array=($(python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu| awk '{print $4}'|xargs python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm |awk '{print $12}'))
 echo "sshpass -p ${password_array[3]} ssh -o StrictHostKeyChecking=no   oushu@${ip_array[3]}"
-sshpass -p ${password_array[3]}  ssh oushu@${ip_array[3]}
+sshpass -p ${password_array[3]}  ssh -o StrictHostKeyChecking=no oushu@${ip_array[3]}
 }
 
 function latestvm {
 id=`cat $ci_dir/setup_machine/clusterid`
-：
 }
+
+function reinit {
+ps -ef | grep postgres | grep -v grep | awk '{print $2}'| xargs kill -9
+echo "**************************free****************************"
+free_shared_mem
+oushudb ps
+rm -rf ~/hawq-data-directory/*
+rm -rf /usr/local/oushu/log/oushudb/*
+oushudb init cluster -a
+}
+
+function pull_all_master {
+ cd ~/oushudb/hornet-opensource
+ git checkout master
+ git pull
+ git checkout -
+ cd ../hornet
+ git checkout master
+ git pull
+ git checkout -
+ cd ../hawq 
+ git checkout oushu-master
+ git pull
+ git checkout -
+}
+function lldb_latest {
+lldb -c `ls -lat /cores/*|head -n1|awk '{print $9}'`
+}
+
+function make_opensource {
+	cd ~/oushudb/hornet-opensource/build
+	make -j
+}
+function make_hornet {
+	cd ~/oushudb/hornet/build
+	make -j
+}
+
+function make_hawq {
+	cd ~/oushudb/hawq
+	make -j
+}
+function make_debug_opensource {
+	cd ~/oushudb/hornet-opensource/build
+	make debug -j
+}
+function make_debug_hornet {
+	cd ~/oushudb/hornet/build
+	make debug -j
+}
+
+function make_debug_hawq {
+	cd ~/oushudb/hawq
+	make  -j
+}
+
+
+function f ()
+{
+ ps -caxm -orss= | awk '{ sum += $1 } END { print "Resident Set Size: " sum/1024 " MiB" }'
+}
+
+function git_remove_proxy ()
+{
+  git config --global --unset http.proxy
+  git config --global --unset https.proxy
+  git config --global  --list
+}
+function git_add_proxy ()
+{
+  git config --global   http.proxy 127.0.0.1:4780
+  git config --global  https.proxy 127.0.0.1:4780
+  git config --global  --list
+}
+
+function git_remove_branch ()
+{
+  localbname=`git branch -a |grep lll|grep -v '/origin/'`
+  for bname in ${localbname};
+  do
+     git branch -D $bname
+  done
+  remotebname=`git branch -a |grep '/origin/lll'`
+  for bname in ${remotebname};
+  do
+    bname=`echo -n $bname|awk -F/ '{print $3}'`
+    git push origin --delete $bname
+  done
+}
+
+function create_branch ()
+{
+ cd ~/oushudb/hawq
+ git checkout oushu-master
+ git checkout -b $1 
+ cd ~/oushudb/hornet
+ git checkout master
+ git checkout -b $1 
+ cd ~/oushudb/hornet-opensource
+ git checkout master
+ git checkout -b $1 
+}
+function checkout_branch ()
+{
+ branch_name=$1 
+ hawq_branch_name=$1 
+ cd ~/oushudb/hawq
+ result=$(echo ${branch_name} | grep "master")
+ if [[ "${result}" != "" ]]
+ then
+     hawq_branch_name="oushu-master"
+     branch_name="master"
+ fi
+ git checkout ${hawq_branch_name}
+ cd ~/oushudb/hornet
+ git checkout ${branch_name}
+ cd ~/oushudb/hornet-opensource
+ git checkout ${branch_name}
+}
+function ftest ()
+{
+	if [ -f "/Users/lululi/oushudb/hawq/src/test/feature/feature-test" ];
+	then 
+		/Users/lululi/oushudb/hawq/src/test/feature/feature-test --gtest_filter=$1
+	else
+		cd /Users/lululi/oushudb/hawq/
+		make feature-test
+		/Users/lululi/oushudb/hawq/src/test/feature/feature-test --gtest_filter=$1
+	fi
+}	
--- .bashrc
+++ .bashrc
@@ -2,14 +2,20 @@ ulimit -n 65536
 ulimit -c 10000000000
 export CC=clang
 export CXX=clang++
+export HIVE_HOME=/Users/lululi/apps/hive
 export DEPENDENCY_PATH=/opt/dependency/package
 source /opt/dependency-Darwin/package/env.sh
 
 export HADOOP_HOME=/usr/local/Cellar/hadoop/3.3.1/libexec 
-export PATH=$PATH:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin
-export JAVA_HOME=/usr/local/Cellar/openjdk@8/1.8.0+312/libexec/openjdk.jdk/Contents/Home
-source /usr/local/oushu/oushudb/oushudb_path.sh
-export DYLD_LIBRARY_PATH=/opt/dependency-Darwin/package/lib:$DYLD_LIBRARY_PATH
+export PATH=$PATH:${HADOOP_HOME}/bin:${HADOOP_HOME}/sbin:${HIVE_HOME}/bin
+export JAVA_HOME=/usr/local/Cellar/openjdk@8/1.8.0+322/libexec/openjdk.jdk/Contents/Home
+export dbversion='50'
+if [ "$dbversion" = '50' ];then 
+	source /usr/local/oushu/oushudb/oushudb_path.sh
+elif [ "$dbversion" = '49' ];then
+	source /usr/local/hawq/greenplum_path.sh
+	alias oushudb='hawq'
+fi
 alias updatedb='sudo /usr/libexec/locate.updatedb'
 alias vimrc='vim ~/.vimrc'
 alias bashrc='vim ~/.bashrc'
@@ -18,21 +24,52 @@ alias source_rc='source  ~/.bashrc'
 alias oushu_init='oushudb init cluster --with_magma -a'
 source /Users/lululi/.bash_function.sh
 source /Users/lululi/.git-completion.bash
+TEST_DB_NAME="postgres"
 
 mkdir -p /tmp/magma_catalog
 mkdir -p /tmp/magma_data
 alias lgt="ssh lululi@101.43.188.6"
 alias oushu='cd /usr/local/oushu/'
-export PATH=/usr/local/oushu/lavam/bin/:$PATH
+export PATH=/usr/local/oushu/lavam/bin/:/usr/local/mysql/bin/:$PATH
 export LAVA_USER="lilulu@oushutest"
 export LAVA_PASSWORD="qminet8r5h"
 alias startvm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --multicreate 3,3"
 alias listvm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lulu|awk '{print $4}'"
+alias listall="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list"
+alias listlll="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lll"
 alias listci_vm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --list|grep lll|awk '{print $4}'"
 alias startvm_debug="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --multicreate 3,3 --testbinary debug"
-export PATH=/usr/local/oushu/lavam/bin:$PATH
 alias ci='cd /Users/lululi/apps/ci-publiccloud/setup_machine/python/'
 alias vm="python /Users/lululi/apps/ci-publiccloud/setup_machine/python/setup.py --listvm"
 export ci_dir="/Users/lululi/apps/ci-publiccloud/"
 export RUN_UNITTEST=NO
-export PATH=/usr/local/oushu/lavam/bin:$PATH
+alias lldb_postgres='ps -ef'
+alias conf="cd /usr/local/oushu/conf/oushudb"
+alias conf="cd /usr/local/oushu/log/oushudb"
+alias buildandrun='cd ~/oushudb/hornet-opensource/build && make debug -j8 && cd ../../hornet/build && make debug -j8 && cd ../../hawq && make -j8 && make -j8 install && reinitmagma'
+export PATH="/usr/local/opt/libtool/libexec/gnubin:$PATH"
+export HEAP_PROFILE_ALLOCATION_INTERVAL=1048576
+alias home='ssh van@192.168.1.222'
+alias kpadmin="ssh gpadmin@192.168.1.163"
+alias kpoushu="ssh oushu@192.168.1.163"
+alias tb='ssh gpadmin@52.131.104.81 -p 1233'
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+export PGDATABASE=postgres
+export HADOOP_CONF_DIR=/usr/local/Cellar/hadoop/3.3.1/libexec/etc/hadoop/
+export http_proxy=http://127.0.0.1:4780
+export https_proxy=http://127.0.0.1:4780
+export ALL_PROXY=socks5://127.0.0.1:4781
+#alias find-merge="!sh -c 'commit=$0 && branch=${1:-HEAD} && (git rev-list $commit..$branch --ancestry-path | cat -n; git rev-list $commit..$branch --first-parent | cat -n) | sort -k2 -s | uniq -f1 -d | sort -n | tail -1 | cut -f2'"
+#alias show-merge="!sh -c 'merge=$(git find-merge $0 $1) && [ -n \"$merge\" ] && git show $merge'"
+alias cdhawq='cd ~/oushudb/hawq'
+alias cdhornet='cd ~/oushudb/hornet'
+alias cdopensource='cd ~/oushudb/hornet-opensource'
+alias mkcd='mkdir -p  build && cd build'
+alias makecleanall='cdopensource && git clean -xdf && mkcd && ../bootstrap&& make debug -j && cdhornet && git clean -xdf && mkcd && ../bootstrap && make debug -j&& cdhawq && git clean -xdf && ./configure --enable-debug --enable-cassert && make -j && make install'
+alias makereleasecleanall='cdopensource && git clean -xdf && mkcd && ../bootstrap&& make release -j && cdhornet && git clean -xdf && mkcd && ../bootstrap && make release -j&& cdhawq && git clean -xdf && ./configure  && make -j && make install -j'
+alias restart='oushudb restart cluster -a'
+alias makeall='make_opensource && make_hornet && make_hawq && make install -j8'
+alias makedebugall='make_debug_opensource && make_debug_hornet && make_debug_hawq && make install -j8'
+alias kp3='sshpass -p Admin@220817 ssh -o StrictHostKeyChecking=no -p 3473 oushu@42.193.136.114'
+pass=Pj8fUeosnH4PfIdpR9
+alias ftest='cd /Users/lululi/oushudb/hawq/src/test/feature/'
